05b557a54b98242f54f9c57df901f88bcd1cabe7
Nikhil Solanke <nikhilsolanke5@gmail.com>
2024-04-11 17:08:25 +0530

    new parser

--- parser.py
+++ parser.py
@@ -1,30 +1,46 @@
 from poppler import load_from_file
 import re
-import xlsxwriter
+
 
 def parse(pdffile):
     patterns = {
-        "seat_no": r"Seat no:.*?(\d+)",
-        "name": r"Name:.*?([\w\W]+) mother",
-        "mother_name": r"mother name:.*?(\w+)",
-        "marks": r"\d{7}.*",
+        "seat_no": r"Seat No:.*?(\w+)",
+        "name": r"Name:\s+?([\w]+?)\s+",
+        "mother_name": r" Mother’s Name:[\s]*?([\w]+?)\s+[\w]",
+        "marks": r"\w{6}\s*\w*[\W\w]*",
     }
 
-    data = {}
+    data: list[dict] = []
 
     pdf = load_from_file(pdffile)
-    page = pdf.create_page(0)
+    for count in range(pdf.pages):
+        dat = {}
+        page = pdf.create_page(count)
+        txt = page.text()
 
-    txt = page.text()
+        dat["Seat No"] = re.findall(patterns["seat_no"], txt)[0].strip()
+        dat["Name"] = re.findall(patterns["name"], txt)[0].strip()
+        dat["Mother's Name"] = re.findall(patterns["mother_name"], txt)[0].strip()
 
-    data["Seat No"] = re.findall(patterns["seat_no"], txt)[0].strip()
-    data["Name"] = re.findall(patterns["name"], txt)[0].strip()
-    data["Mother's Name"] = re.findall(patterns["mother_name"], txt)[0].strip()
+        marks_dat: list[tuple] = []
+        for line in txt.splitlines():
+            stuff = re.findall(r"^(\d{6})\s*\w*\s*[\w\-]*\s*\s*[\w\-]*\s*\s*[\w\-]*\s*\s*[\w\-/]*\s*\s*[\w\-]*\s*\s*[\w\-]*\s*\s*[\w\-]*\s*\s*([\w\-]*)\s*\s*[\w\-]*\s*\s*([\w\-]*)\s*\s*[\w\-]*\s*\s*[\w\-]*\s*\s*[\w\-]*\s*\s*[\w\-]*", line.strip())
+            if stuff:
+                marks_dat.append(stuff[0])
 
-    marks_data = list(map(lambda x: x.split(), re.findall(patterns["marks"], txt)))
+        dat["result"] = {}
 
-    print(data)
-    print("\n".join(map(str, marks_data)))
+        for mark in marks_dat:
+            subject, tot, grade = mark
+            dat["result"][subject] = {}
+            dat["result"][subject]["Total%"] = tot
+            dat["result"][subject]["Grade"] = grade
+
+        data.append(dat)
 
     return data
 
+
+if __name__ == "__main__":
+    data = parse("gadget.pdf")
+    print(data)
